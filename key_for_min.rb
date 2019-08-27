4d7e1721190e379d5a086d8c719adffbcaa8dfa3
Joseph Sesti <sestijoey63@gmail.com>
2019-08-27 18:03:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,11 @@ def key_for_min_value(name_hash)
   key = nil
   value = nil
   name_hash.each do |a,b|
-    binding.pry
     if value == nil || value > b
       value = b
       key = a
     end
+    binding.pry
   end
   key
 end
